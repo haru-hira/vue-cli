cc9bffbe2c05d73726a972a0f2b585e365b87b4d
admin_haru <moto55.haru13@gmail.com>
2020-07-06 20:50:02 +0900

    added emit.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import LikeNumber from './components/LikeNumber.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      number: 14
+    };
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    LikeNumber
+  },
+  methods: {
+    incrementNumber(value) {
+      this.number = value;
+    }
   }
 }
 </script>
